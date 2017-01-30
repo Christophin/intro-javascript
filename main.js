0fd0aa6df5c54ba98a4f31d12e0988ea348b4581
Chris Coralli <ccoralli@gmail.com>
2017-01-30 12:59:08 -0500

    Completed FizzBuzz.

--- main.js
+++ main.js
@@ -2,8 +2,19 @@ var resultsDiv = document.getElementById("results");
 var textBox = document.getElementById("fizz-num");
 var button = document.getElementById("submit-button");
 
-function fizzbuzz (n) {
+function fizzBuzz (n) {
+  var num = Number(textBox.value);
+  for (var count = 1; count < num; count++)  {
+    if (count % 15 === 0) {
+      resultsDiv.innerHTML += ("<p>FizzBuzz</p>")
+    } else if (count % 5 === 0) {
+      resultsDiv.innerHTML += ("<p>Buzz</p>")
+    } else if (count % 3 === 0) {
+      resultsDiv.innerHTML += ("<p>Fizz</p>")
+    } else {
+      resultsDiv.innerHTML += "<p>" + count + "</p>"
+    };
+  };
+};
 
-}
-
-button.addEventlistener('click', fizzbuzz);
+button.addEventListener('click', fizzBuzz);
