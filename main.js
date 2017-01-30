73e72b45e57ba7d60a7120d60ca59cb13a28ba9a
Chris Coralli <ccoralli@gmail.com>
2017-01-30 12:15:42 -0500

    fizzbuzz function wired to button.

---
+++ main.js
@@ -0,0 +1,9 @@
+var resultsDiv = document.getElementById("results");
+var textBox = document.getElementById("fizz-num");
+var button = document.getElementById("submit-button");
+
+function fizzbuzz (n) {
+
+}
+
+button.addEventlistener('click', fizzbuzz);
